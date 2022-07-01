f22486d07bb36b78d25f2a366172125d1c1db6f4
Mohit Patel <45488208+mohit-design@users.noreply.github.com>
2022-07-01 21:22:44 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,9 +4,44 @@ $(document).ready(function() {
 	  if ($(window).scrollTop() >= 50) {
 	    $(".bg-logo-menu-desktop").addClass("active");
       $(".menu-listing-desktop").addClass("active");
+			$(".img-logo-header-desktop").attr("src", "../images/logo.png"); 
 	   } else {
 	    $(".bg-logo-menu-desktop").removeClass("active");
       $(".menu-listing-desktop").removeClass("active");
+			$(".img-logo-header-desktop").attr("src", "../images/logo-white.png");
 	   }
 	});
 });
+// Window Loader JS
+;(function(){
+  function id(v){return document.getElementById(v); }
+  function loadbar() {
+    var ovrl = id("overlay"),
+        prog = id("progress"),
+        stat = id("progstat"),
+        img = document.images,
+        c = 0;
+        tot = img.length;
+
+    function imgLoaded(){
+      c += 1;
+      var perc = ((100/tot*c) << 0) +"%";
+      prog.style.width = perc;
+      stat.innerHTML = "Loading "+ perc;
+      if(c===tot) return doneLoading();
+    }
+    function doneLoading(){
+      ovrl.style.opacity = 0;
+      setTimeout(function(){ 
+        ovrl.style.display = "none";
+      }, 1200);
+    }
+    for(var i=0; i<tot; i++) {
+      var tImg     = new Image();
+      tImg.onload  = imgLoaded;
+      tImg.onerror = imgLoaded;
+      tImg.src     = img[i].src;
+    }    
+  }
+  document.addEventListener('DOMContentLoaded', loadbar, false);
+}());
